b0ab28d28c96e83ca336b7bcefaa4f4bc5683584
amonaco18 <alexandermonaco18@gmail.com>
2020-07-20 21:03:34 -0400

    Switched to clearDB mySQL database hosted on Heroku.

--- capstone_app/plant_tracker/models.py
+++ capstone_app/plant_tracker/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Example(models.Model):
-    rand_attr = models.CharField(max_length=12)
+class User(models.Model):
+    user_id = models.AutoField(primary_key = True)
+    user_name = models.CharField(max_length=30)
+    passwd = models.CharField(max_length=30)
+
