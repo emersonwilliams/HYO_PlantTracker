4179ae6e2411feba224ed927a8effd8fa2c28dd9
May Li <mayli@macbook-pro.lan>
2020-07-23 17:04:39 -0700

    Added placeholder html files to the templates folder and modified urls.py and views.py to handle elementary requests i.e. /my-plants or /my-plants/2/

--- capstone_app/plant_tracker/urls.py
+++ capstone_app/plant_tracker/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # ex: /add-plant
+    path('add-plant', views.addplant, name='addplant'),
+    # ex: /my-plants
+    path('my-plants', views.myplants, name='myplants'),
+    # ex: /my-plants/sunny-the-sunflower
+    path('my-plants/<int:plant_id>/', views.plantdetail, name='plant-detail')
+    # NOTE: the above assumes we're taking in a key-word called plant-id
 ]
 
--- capstone_app/plant_tracker/views.py
+++ capstone_app/plant_tracker/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+# ...
+from django.http import HttpResponse
+import datetime
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def myplants(request):
+    return render(request, 'myplants.html', {})
+
+def addplant(request):
+    return render(request, 'addplant.html', {})
+
+def plantdetail(request, plant_id):
+    # Need to add stuff to handle actually getting and displaying the detail !
+    return render(request, 'plantdetail.html', {})
