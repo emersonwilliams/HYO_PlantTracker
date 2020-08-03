e5a45c216f78fdea2624dcec4e6332cae5ac92b8
Alex M <alexandermonaco18@gmail.com>
2020-08-03 16:11:02 -0400

    Customized user model/ Registration system
    Re-initialized database to reflect changes to db schema, and linked registration/login systems.

--- capstone_app/plant_tracker/views.py
+++ capstone_app/plant_tracker/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 # ...
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 import datetime
 from django.views.decorators.csrf import csrf_exempt
-from . import models
+from . import models, forms
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
 @csrf_exempt
 def home(request):
@@ -13,14 +15,19 @@ def home(request):
 @csrf_exempt
 def login(request):
     if request.method == "POST":
+
         #Authenticate is a Django implemented function. It takes credentials as keyword args, and checks for them in the auth_user table. If it verifies
         #the credentials, then it returns a user object, but returns false otherwise.
+
         user = authenticate(username=request.POST.get("username"), password = request.POST.get("password"))
         if user is not None:
+
             #Login is an additional function pre-implemented in Django. The function will store the user objects ID in a session, and I'm hoping it binds a
             #digital signature to the request object for stronger security but I think it just uses a cookie via a sessions table
-            login(request, user)
-            return render(request, "myplants.html")
+
+            auth_login(request, user)
+            return redirect("myplants")
+
         else:
             message = "Login attempt failed. Please try again or register to create an account."
             return render(request, "test_login.html", {"error_message": message})
@@ -29,7 +36,17 @@ def login(request):
 
 @csrf_exempt
 def register(request):
-    return render(request, 'register.html', {})
+    form = forms.RegistrationForm()
+
+    if request.method == "POST":
+        form = forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            print("Saving user..")
+            form.save()
+            return redirect("login")
+
+    context = {"form": form}
+    return render(request, 'register.html', context)
 
 @csrf_exempt
 def myplants(request):
